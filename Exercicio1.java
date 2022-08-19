88e7b4bb827981049dcbecf460b0e081bd42256c
Antonia Alves <antoniaalves242526@gmail.com>
2022-08-19 20:09:10 -0300

    segundo commit

--- Exercicio1.java
+++ Exercicio1.java
@@ -3,12 +3,14 @@ import javax.swing.JOptionPane;
 
 public class Exercicio1 {
     public static void main(String[] args) {
-        double real  = 5.17 ;
-        double valorDolar, resultado ;
-        valorDolar = Double.parseDouble(JOptionPane.showInputDialog("Digite a quantidade dolares: "));
-        resultado =  valorDolar *  real ;
-
-        System.out.println(valorDolar + " dolares equivale a R$" + resultado);
+        
+       
+        double dolar , cotacao, real; 
+        dolar = Double.parseDouble(JOptionPane.showInputDialog("digite a quantidade dee dolares:"));
+        cotacao = Double.parseDouble(JOptionPane.showInputDialog("valor do dolar")); 
+        real = dolar * cotacao ;
+                
+        JOptionPane.showMessageDialog(null, "Voce tem R$" + real);
 
     }
 }
